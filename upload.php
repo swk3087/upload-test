4147c139c7747a3c67251821ca72b3f7bf9b6cd9
swk3087 <89524975+swk3087@users.noreply.github.com>
2022-11-04 19:57:36 +0900

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,5 +1,7 @@
 <?php
-$source = $_FILES['profile']['tmp_name']; $dest="./".basename($_FILES['profile']['name']); move_uploaded_file($source, $dest);
+$source = $_FILES['profile']['tmp_name']; 
+$dest="./".basename($_FILES['profile']['name']); 
+move_uploaded_file($source, $dest);
 ?>
 <html>
   <head>
